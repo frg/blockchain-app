b35347146e41eabe5fc4f9ac871e8d08d565170d
Jean Farrugia <jeanfrg@gmail.com>
2023-03-31 18:39:59 +0200

    chore: removed up time code

--- src/utils/time.ts
+++
@@ -1,6 +0,0 @@
-import { UnixTimestampToDateTime } from '../../types/utils/time';
-
-export const unixTimestampToDateTime: UnixTimestampToDateTime = (timestamp) => {
-    const milliseconds = timestamp * 1000
-    return new Date(milliseconds);
-};
