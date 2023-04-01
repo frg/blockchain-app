3a7184a066b79dce81f415a5ed5ee2e9c6aef5eb
Jean Farrugia <jeanfrg@gmail.com>
2023-04-01 11:34:27 +0200

    chore: refactored state

--- types/utils/state.d.ts
+++ types/utils/state.d.ts
@@ -1,20 +1,13 @@
-export const enum States {
-    Default = "uknown",
-    Success = "success",
-    Error = "error",
-    NotFound = "not found"
-};
-
 export type SuccessState<T> = {
-    state: States.Success;
+    state: "success";
     data: T;
 };
 
 export type ErrorState = {
-    state: States.Error;
+    state: "error";
     error: "";
 };
 
 export type NotFoundState = {
-    state: States.NotFound;
+    state: "not found";
 };
